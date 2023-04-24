2b8a096e0db24fea82e9dc1a12edc359207dc8a7
FallingKids <739820174@qq.com>
2023-04-24 20:03:17 +0800

    chore(release): add rechart

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,11 +1,39 @@
 import DashboardBox from "@/components/DashboardBox";
-
+import { Stock } from "@ant-design/plots";
+import { useState, useEffect } from "react";
+import { ResponsiveContainer } from "recharts";
 type Props = {};
 
 const Row2 = (props: Props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch("https://gw.alipayobjects.com/os/antfincdn/ZWgtj7pC%261/stock.json")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+  const config = {
+    appendPadding: [0, 20, 20, 20],
+    data,
+    xField: "trade_date",
+    yField: ["open", "close", "high", "low"],
+    slider: {},
+  };
+
   return (
     <>
-      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="d">
+        <ResponsiveContainer width="100%" height="100%">
+          <Stock {...config} />
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox gridArea="e"></DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
+import { useEffect } from "react";
 
 type Props = {};
 
@@ -52,6 +53,9 @@ const gridTemplateSmallScreens = `
 
 const Dashboard = (props: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  useEffect(() => {
+    document.title = '巨丰量化 - dashboard';
+  }, []);
   return (
     <Box
       width="100%"
