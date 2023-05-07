f7a1024fa4247c78e62ea067c7b5bf69858122f5
huyz <739820174@qq.com>
2023-05-07 14:36:42 +0800

    meta:1. finish prediction

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,18 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {
-    GetKpisResponse,
-  } from "./types";
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-    reducerPath: 'main',
-    tagTypes: ['Kpis'],
-    endpoints: (build) => ({
-        getKpis: build.query<Array<GetKpisResponse>, void>({
-            query: () => 'kpi/kpis/',
-            providesTags: ['Kpis']
-        })
-    })
-})
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products", "Transactions"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
+    }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+  }),
+});
 
-export const { useGetKpisQuery } = api
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } = api;
--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
-// import productRoutes from "./routes/product.js";
-// import transactionRoutes from "./routes/transaction.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
-// import Product from "./models/Product.js";
-// import Transaction from "./models/Transaction.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
 import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
@@ -26,8 +26,8 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
-// app.use("/product", productRoutes);
-// app.use("/transaction", transactionRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -38,7 +38,7 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    
+
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
